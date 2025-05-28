5e029a60989a55736ef8c2055a73d9c90e3a44cf
MROS <yc1043@gmail.com>
2025-05-28 22:55:38 +0800

    啟用 import std

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,16 @@
-set_xmakever("2.7.0")
+-- 建置指令 --sdk 指向 llvm 目錄，需版本 19
+-- xmake config --toolchain=llvm --runtimes=c++_shared --sdk=/opt/llvm-19
+-- xmake build
+-- xmake run
+
+set_xmakever("2.9.9")
 
 -- 設置項目信息
 set_project("鼎爐")
 set_version("1.0.0")
 
 -- 設置 C++ 標準
-set_languages("c++20")
+set_languages("c++23")
 
 -- 目標定義
 target("hello_world")
